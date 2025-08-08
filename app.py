7a3457a0840570966718819a9355048a2fc5cc96
¨Santiago <santiago-serranop@outlook.com>
2025-08-08 17:31:31 -0500

    final

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
- st.write('Graficos de dispersion e histograma para el conjunto de datos de anuncios de venta de coches')
+st.title('Graficos de dispersion e histograma para el conjunto de datos de anuncios de venta de coches')
 car_data = pd.read_csv('dataset/vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')    # crear un botón
 
